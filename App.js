3a7bec1a48ab631b228721de6ae1d63ad0227048
marinusa torres <henriquepro8@gmail.com>
2024-06-10 03:35:05 -0300

    botao

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 // import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -14,59 +14,35 @@ import MainMenu from './screens/MainMenu';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TransitionPresets } from '@react-navigation/native-stack';
 
-const Tab = createBottomTabNavigator();
-
-const Stack= createNativeStackNavigator();
-
-function shouldShowHeader(route) {
-  // Verifica se a rota atual é diferente das telas de login, registro e esqueci minha senha
-  return route.name !== 'Login' && route.name !== 'Register' && route.name !== 'ForgotPassword';
-}
 
 export default function App() {
+
+
+  const [status, setStatus] = useState('On');
+  const [bg, setBg] = useState('bg-green-500');
+
+  const click = () => {
+    if (status == "On"){
+      setStatus('Off')
+      setBg('bg-red-500')
+    }
+    if (status == "Off"){
+      setStatus('On')
+      setBg('bg-green-500')
+    }
+  }
   
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    // Simulate loading process
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Adjust the time as needed
-  }, []);
-  
+   return (
+    <>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <TouchableOpacity className={`border-2 w-32 h-32 rounded-full items-center justify-center ${bg}`} onPress={click}>
+
+          <Text>{status}</Text>
+      </TouchableOpacity>
     
-  
-
-  
-
-
-  return (
-    <NavigationContainer>
-    {isLoading ? <SplashScreen /> : <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarStyle: { backgroundColor: '#ff5f01' }, // Defina a cor de fundo da barra de navegação
-        headerStyle: { backgroundColor: '#ff5f01' }, // Defina a cor de fundo do cabeçalho
-        headerTitleStyle: { color: 'white' },
-        tabBarShowLabel: false,
-        headerShown: shouldShowHeader(route),
-      })}
-    >
-
-
-
-        <Tab.Screen name="Login" component={LoginScreen} options={{
-                tabBarButton: () => null, // Isso faz com que a tela seja invisível no tab bar
-              }}/>
-        <Tab.Screen name="Register" component={RegisterScreen} options={{
-                tabBarButton: () => null, // Isso faz com que a tela seja invisível no tab bar
-              }}/>
-        <Tab.Screen name="ForgotPassword" component={ForgotPassword} options={{
-                tabBarButton: () => null, // Isso faz com que a tela seja invisível no tab bar
-              }} />
-        <Tab.Screen name="Menu" component={MainMenu} />
-        
-      </Tab.Navigator>}
+    </SafeAreaView>
       
-    </NavigationContainer>
+    </>
   );
 }
 
